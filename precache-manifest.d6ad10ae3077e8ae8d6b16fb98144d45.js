011272f8429da6c62c400d83a035e35537fd5e06
upchm <Michael.Upchurch@pega.com>
2019-06-12 13:58:22 -0400

    Updates

---
+++ precache-manifest.d6ad10ae3077e8ae8d6b16fb98144d45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a53fc6249b79f104cbac9b3be594522",
+    "url": "/updog/index.html"
+  },
+  {
+    "revision": "6d143d5dfb5f9b6f6474",
+    "url": "/updog/static/css/main.408cccf5.chunk.css"
+  },
+  {
+    "revision": "ab35c006568d06a2d1ca",
+    "url": "/updog/static/js/2.a2fc8bd5.chunk.js"
+  },
+  {
+    "revision": "6d143d5dfb5f9b6f6474",
+    "url": "/updog/static/js/main.487329d5.chunk.js"
+  },
+  {
+    "revision": "88b8a7ba382e52ab828d",
+    "url": "/updog/static/js/runtime~main.013c9109.js"
+  }
+]);
